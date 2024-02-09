113930d03adefccb43d1b17c4a6da79141eefa8b
Ciprian <cozma_ciprian19@yahoo.com>
2024-02-10 01:06:42 +0200

    added another solution for ex 18 on beginner

--- beginner/18.getMonthOfTheYear.js
+++ beginner/18.getMonthOfTheYear.js
@@ -1,20 +1,24 @@
 function getMonthOfTheYear(date) {
-  const month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const monthIdx = date.getMonth();
-  return month[monthIdx];
+  // const month = [
+  //   "January",
+  //   "February",
+  //   "March",
+  //   "April",
+  //   "May",
+  //   "June",
+  //   "July",
+  //   "August",
+  //   "September",
+  //   "October",
+  //   "November",
+  //   "December",
+  // ];
+  // const monthIdx = date.getMonth();
+  // return month[monthIdx];
+
+  return new Intl.DateTimeFormat("es-ES", {
+    month: "long",
+  }).format(date);
 }
 
 const month = new Date("2024-12-25T16:15:00.000Z");
